f72b01dae0ad14a69f5e60291408a9cbd03f56b5
Samuel Chan <cchin.samuel@gmail.com>
2022-06-05 21:00:33 +0800

    fix: strange issue of missing collection groupby

--- device_preview/lib/src/views/tool_panel/sections/subsections/device_model.dart
+++ device_preview/lib/src/views/tool_panel/sections/subsections/device_model.dart
@@ -106,6 +106,14 @@ class _PlatformModelPicker extends StatelessWidget {
 
   final TargetPlatform platform;
 
+  Map<T, List<S>> groupBy<S, T>(Iterable<S> values, T Function(S) key) {
+    var map = <T, List<S>>{};
+    for (var element in values) {
+      (map[key(element)] ??= []).add(element);
+    }
+    return map;
+  }
+
   @override
   Widget build(BuildContext context) {
     final devices = context.select(
